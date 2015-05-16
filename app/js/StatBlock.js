dd6cb8f2295b95648c6c6df7a9d4ef60ab3a07c6
Christian McNeill <christian@redxian.com>
2015-05-16 10:06:24 +1000

    Created template files for Statblock

--- app/js/StatBlock.js
+++ app/js/StatBlock.js
@@ -2,70 +2,7 @@ angular.module("characterSheet.statBlock", [])
     .directive("statBlock", function() {
         return {
             restrict: 'E',
-            templateUrl: "partials/stat-block.html",
-            controller: function($scope, CharacterFactory, AbilityFactory, SkillFactory) {
-                $scope.abilities = AbilityFactory;
-                $scope.character = CharacterFactory;
-                $scope.skills = SkillFactory;
-
-                $scope.armor = {
-                    armorClass: function() {
-                        return 10;
-                    },
-                    touchAC: function() {
-                        return 10;
-                    },
-                    flatFooted: function() {
-                        return 10;
-                    },
-                    bonuses: [{
-                        name: "armor",
-                        value: 0
-                    }, {
-                        name: "deflection",
-                        value: 0
-                    }, {
-                        name: "Dex",
-                        value: 0
-                    }, {
-                        name: "dodge",
-                        value: 0
-                    }, {
-                        name: "insight",
-                        value: 0
-                    }, {
-                        name: "natural",
-                        value: 1
-                    }, {
-                        name: "shield",
-                        value: 0
-                    }, {
-                        name: "monk",
-                        value: 0
-                    }, {
-                        name: "rage",
-                        value: 0
-                    }, {
-                        name: "size",
-                        value: 0
-                    }, {
-                        name: "Wis",
-                        value: 0
-                    }]
-                };
-
-                $scope.spellsPrepared = [
-                    [{
-                        name: "detect magic"
-                    }, {
-                        name: "light"
-                    }, {
-                        name: "mage hand"
-                    }, {
-                        name: "read magic"
-                    }]
-                ];
-            }
+            templateUrl: "partials/stat-block.html"
         };
     })
     .directive("statBlockDefence", function() {
@@ -79,9 +16,141 @@ angular.module("characterSheet.statBlock", [])
             restrict: 'E',
             templateUrl: "partials/stat-block/stat-block-offence.html"
         }
-    }).directive("statBlockStatistics", function() {
+    })
+    .directive("statBlockStatistics", function() {
         return {
             restrict: 'E',
             templateUrl: "partials/stat-block/stat-block-statistics.html"
         }
-    });
+    })
+    .directive("statisticsAbilityScore", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/statistics/statistics-ability-scores.html"
+        }
+    })
+    .directive("statisticsFeats", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/statistics/statistics-feats.html"
+        }
+    })
+    .directive("statisticsGear", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/statistics/statistics-gear.html"
+        }
+    })
+    .directive("statisticsLanguages", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/statistics/statistics-languages.html"
+        }
+    })
+    .directive("statisticsSkills", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/statistics/statistics-skills.html"
+        }
+    })
+    .directive("statisticsSpecialQualities", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/statistics/statistics-special-qualities.html"
+        }
+    })
+    .directive("offenceMelee", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/offence/offence-melee.html"
+        }
+    })
+    .directive("offenceRanged", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/offence/offence-ranged.html"
+        }
+    })
+    .directive("offenceOppositionSchools", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/offence/offence-opposition-school.html"
+        }
+    })
+    .directive("offenceSpecialAttacks", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/offence/offence-special-attacks.html"
+        }
+    })
+    .directive("offenceSpeed", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/offence/offence-speed.html"
+        }
+    })
+    .directive("offenceSpellLikeAbilities", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/offence/offence-spell-like-abilities.html"
+        }
+    })
+    .directive("offenceSpellsPrepared", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/offence/offence-spells-prepared.html",
+            controller: function($scope) {
+              $scope.spellsPrepared = [
+                  { name: "detect magic" },
+                  { name: "light" },
+                  { name: "mage hand" },
+                  { name: "read magic" }
+              ];
+            }
+        }
+    })
+    .directive("defenceArmorClass", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/defence/defence-armor-class.html",
+            controller: function($scope) {
+                $scope.armor = {
+                    armorClass: function() { return 10; },
+                    touchAC: function() { return 10; },
+                    flatFooted: function() { return 10; },
+                    bonuses: [
+                        { name: "armor", value: 0 },
+                        { name: "deflection", value: 0 },
+                        { name: "Dex", value: 0 },
+                        { name: "dodge", value: 0 },
+                        { name: "insight", value: 0 },
+                        { name: "natural", value: 1 },
+                        { name: "shield", value: 0 },
+                        { name: "monk", value: 0 },
+                        { name: "rage", value: 0 },
+                        { name: "size", value: 0 },
+                        { name: "Wis", value: 0 }
+                    ]
+                };
+            }
+        }
+    })
+    .directive("defenceDefensiveAbilities", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/defence/defence-defensive-abilities.html"
+        }
+    })
+    .directive("defenceHitPoints", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/defence/defence-hit-points.html"
+        }
+    })
+    .directive("defenceSavingThrows", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "partials/stat-block/defence/defence-saving-throws.html"
+        }
+    })
+    ;
